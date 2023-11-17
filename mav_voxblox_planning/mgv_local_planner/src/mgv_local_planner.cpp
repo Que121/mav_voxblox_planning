9440fae3b9654dfc792b0773f5d8ee93481d9834
Que121 <qh13005968844@gmail.com>
2023-11-17 16:59:13 +0800

    que

--- mav_voxblox_planning/mgv_local_planner/src/mgv_local_planner.cpp
+++ mav_voxblox_planning/mgv_local_planner/src/mgv_local_planner.cpp
@@ -2,7 +2,7 @@
 #include <mav_trajectory_generation/trajectory_sampling.h>
 
 #include "mgv_local_planner/mgv_local_planner.h"
-#include "../../../install/mav_msgs/include/mav_msgs/conversions.h"// 消息类型需要更改
+#include "../../../install/mav_msgs/include/mav_msgs/conversions.h" // 消息类型需要更改
 
 namespace mgv_planning
 {
@@ -58,7 +58,8 @@ namespace mgv_planning
     yaw_policy_.setYawPolicy(YawPolicy::PolicyType::kVelocityVector); // 设置偏航策略
   }
 
-  void MgvLocalPlanner::odometryCallback_mgv(const mgv_msgs::Odometry &msg)
+  // 注意odometry为nav，此为自动生成的消息类型文件
+  void MgvLocalPlanner::odometryCallback_mgv(const nav_msgs::Odometry &msg)
   {
     // 消息类型需要更改
     mgv_msgs::eigenOdometryFromMsg(msg, &odometryOFmgv_); // 将ros消息转换为eigen类型的变量
@@ -150,7 +151,7 @@ namespace mgv_planning
                timer.stop());
 
       // 可视化路径
-      visualizePath();
+      // visualizePath();
     }
   }
 
@@ -159,15 +160,11 @@ namespace mgv_planning
       const mgv_msgs::EigenTrajectoryPointVector &waypointsOFmgv,
       mgv_msgs::EigenTrajectoryPointVector *pathOFmgv)
   {
-    // 确保传入的 'path' 不为空
-    CHECK_NOTNULL(pathOFmgv);
-    // 初始化success变量
-    bool success = false;
-    if (smoother_name_ == "loco")
-    // 选择的算法为loco
+    CHECK_NOTNULL(pathOFmgv);     // 确保path不为空
+    bool success = false;         // 初始化success变量 为规划是否成功的flag
+    if (smoother_name_ == "loco") // 目前只使用loco算法
     {
-      // 判断航点数量
-      if (waypointsOFmgv.size() == 2)
+      if (waypointsOFmgv.size() == 2) // 判断是否有两点用于规划
       {
         // 规划两点之间的路径
         success = loco_smoother_.getPathBetweenTwoPoints(waypointsOFmgv[0],
@@ -177,35 +174,24 @@ namespace mgv_planning
     return success;
   }
 
-  // 取代原来路径点的信息，传入新的路径点信息
+  // 获取新的path
   void MgvLocalPlanner::replacePath_mgv(
       const mgv_msgs::EigenTrajectoryPointVector &pathOFmgv)
   {
-    // 1.上锁  // nm谁
-    std::lock_guard<std::recursive_mutex> guard(path_mutexOFmgv_);
-
-    // 2.清除原来路径点
-    path_queueOFmgv_.clear();
-
-    // 3.获取新的路径点
-    path_queueOFmgv_ = pathOFmgv;
-
-    // 4.设置路径点起始姿态
-    path_queueOFmgv_.front().orientation_W_B = odometryOFmgv_.orientation_W_B;
-
-    // 需要修改
-    // 5.应用姿态策略
-    yaw_policy_.applyPolicyInPlace(&path_queueOFmgv_);
-
-    // 6.重置索引
-    path_indexOFmgv_ = 0;
+    std::lock_guard<std::recursive_mutex> guard(path_mutexOFmgv_);             // 上锁
+    path_queueOFmgv_.clear();                                                  // 清除原来路径点
+    path_queueOFmgv_ = pathOFmgv;                                              // 获取新的path
+    path_queueOFmgv_.front().orientation_W_B = odometryOFmgv_.orientation_W_B; // 设置起始的姿态
+    // ***可能需要更改***
+    yaw_policy_.applyPolicyInPlace(&path_queueOFmgv_); // 应用姿态策略 人话：设置起始的yaw轴
+    path_indexOFmgv_ = 0;                              // 重置索引
   }
 
   // 清除原来轨迹信息
   void MgvLocalPlanner::clearTrajectory_mgv()
   {
     std::lock_guard<std::recursive_mutex> guard(path_mutexOFmgv_);
-    command_publishing_timer_mgv_.stop();
+    command_publishing_timer_mgv_.stop(); // 停止发布命令定时器
     path_queueOFmgv_.clear();
     path_indexOFmgv_ = 0;
   }
@@ -213,100 +199,82 @@ namespace mgv_planning
   // 发布飞行指令
   void MgvLocalPlanner::startPublishingCommands_mgv()
   {
-    // Call the service call to takeover publishing commands.
-    // 1.接管命令发布服务
-    if (position_hold_client_.exists())
+    if (position_hold_client_.exists()) // 接管命令发布服务
     {
       std_srvs::Empty empty_call;
-      // 向无人机发送一个空的服务请求，用于接管命令的发布
-      position_hold_client_.call(empty_call);
+      position_hold_client_.call(empty_call); // 向机器人发送一个空的服务请求，用于接管命令的发布
     }
-
-    // Publish the first set immediately, on this thread.
-    // 2.立刻发布第一组命令
-    commandPublishTimerCallback_mgv(ros::TimerEvent());
-
-    // Need advanced timer options to assign callback queue to this timer.
-    // 3.设定定时器并关联回调函数
+    commandPublishTimerCallback_mgv(ros::TimerEvent()); // 立刻发布第一组命令
     ros::TimerOptions timer_options(
         ros::Duration(command_publishing_dt_),
         boost::bind(&MgvLocalPlanner::commandPublishTimerCallback_mgv, this, _1),
-        &command_publishing_queue_);
-
-    // 4.创建定时器
-    command_publishing_timer_mgv_ = nh_.createTimer(timer_options);
+        &command_publishing_queue_);                                // 设定定时器并关联回调函数
+    command_publishing_timer_mgv_ = nh_.createTimer(timer_options); // 创建定时器
   }
 
   // 定时器回调函数，用于在规划的路径上按照一定频率发布控制命令
   void MgvLocalPlanner::commandPublishTimerCallback_mgv(
       const ros::TimerEvent &event)
   {
-    // 1.路径队列的缓冲区大小
-    constexpr size_t kQueueBuffer = 0;
-
-    // 2.如果路径索引小于路径队列的大小，即还有未到达的路径点
+    constexpr size_t kQueueBuffer = 0; // 设置path的缓冲区大小
     if (path_indexOFmgv_ < path_queueOFmgv_.size())
     {
-      // 锁定线程
-      std::lock_guard<std::recursive_mutex> guard(path_mutexOFmgv_);
-
-      // 3.计算发布的路径点数量
-      //  取决于发布的时间间隔和剩余未到达的路径点数量
+      std::lock_guard<std::recursive_mutex> guard(path_mutexOFmgv_); // 锁定线程
+      // 计算发布的path数量
+      // 取决于发布的时间间隔和剩余未到达的路径点数量
       size_t number_to_publish = std::min<size_t>(
           std::floor(command_publishing_dt_ / constraints_.sampling_dt),
-          path_queueOFmgv_.size() - path_indexOFmgv_);
-
-      // 4.设置路径队列的索引顺序
-      size_t starting_index = 0;
-      if (path_indexOFmgv_ != 0)
+          path_queueOFmgv_.size() - path_indexOFmgv_); // std::floor 向下取整
+      size_t starting_index = 0;                       // 设置path队列的索引顺序
+      if (path_indexOFmgv_ != 0)                       // path的index不为零
       {
-        starting_index = path_indexOFmgv_ + kQueueBuffer;
-        // 如果path_index_不为0，加上KQueueBuffer防止在边界上选择路径点出现问题
+        starting_index = path_indexOFmgv_ + kQueueBuffer; // 如果path_index_不为0，加上KQueueBuffer防止在边界上选择点出现问题
         if (starting_index >= path_queueOFmgv_.size())
         {
-          starting_index = path_indexOFmgv_;
+          starting_index = path_indexOFmgv_; // reset开始的index为当前的index of path
         }
       }
+      // caonima直接发不行阿？？？
+      //=================================================================
+      // size_t number_to_publish_with_buffer = std::min<size_t>(
+      //     number_to_publish + mpc_prediction_horizonOFmgv_ - kQueueBuffer,
+      //     path_queueOFmgv_.size() - starting_index);
+      // mgv_msgs::EigenTrajectoryPointVector::const_iterator first_sample =
+      //     path_queueOFmgv_.begin() + starting_index; // 提取发送path的头
+      // mgv_msgs::EigenTrajectoryPointVector::const_iterator last_sample =
+      //     first_sample + number_to_publish_with_buffer; // 提取发送path的尾
+      // mgv_msgs::EigenTrajectoryPointVector trajectory_to_publish(first_sample,
+      //                                                            last_sample);
+      //=================================================================
 
-      size_t number_to_publish_with_buffer = std::min<size_t>(
-          number_to_publish + mpc_prediction_horizonOFmgv_ - kQueueBuffer,
-          path_queueOFmgv_.size() - starting_index);
-
-      // 提取要发布的路径点
-      mgv_msgs::EigenTrajectoryPointVector::const_iterator first_sample =
-          path_queueOFmgv_.begin() + starting_index;
-      mgv_msgs::EigenTrajectoryPointVector::const_iterator last_sample =
-          first_sample + number_to_publish_with_buffer;
-      mgv_msgs::EigenTrajectoryPointVector trajectory_to_publish(first_sample,
-                                                                 last_sample);
-
-      // 6.创建和发布控制指令信息
+      // 直接从头到尾
+      mgv_msgs::EigenTrajectoryPointVector trajectory_to_publish(path_queueOFmgv_.begin() + starting_index,
+                                                                 path_queueOFmgv_.end());
       trajectory_msgs::MultiDOFJointTrajectory msg; // 创建一个多自由度关节轨迹消息，用于存储控制指令
-      msg.header.frame_id = local_frame_id_;
-      msg.header.stamp = ros::Time::now();
 
-      // 打印发布的控制信息
-      ROS_INFO(
-          "[Mav Local Planner][Command Publish] Publishing %zu samples of %zu. "
-          "Start index: %zu Time: %f Start position: %f Start velocity: %f End "
-          "time: %f End position: %f",
-          trajectory_to_publish.size(), path_queueOFmgv_.size(), starting_index,
-          trajectory_to_publish.front().time_from_start_ns * 1.0e-9,
-          trajectory_to_publish.front().position_W.x(),
-          trajectory_to_publish.front().velocity_W.x(),
-          trajectory_to_publish.back().time_from_start_ns * 1.0e-9,
-          trajectory_to_publish.back().position_W.x());
+      msg.header.frame_id = local_frame_id_; // 将ROS消息（msg）中header的frame_id字段设置为存储在变量local_frame_id_中的值
+      msg.header.stamp = ros::Time::now();   // 将ROS消息（msg）中header的stamp字段设置为当前ROS时间
 
-      // 将 trajectory_to_publish 中的路径点转换为 ROS 的控制指令消息格式。
-      mgv_msgs::msgMultiDofJointTrajectoryFromEigen(trajectory_to_publish, &msg);
-
-      // 8.发布信息
-      command_pub_.publish(msg);
-
-      // 9.更新路径索引并通知重新规划
-      path_indexOFmgv_ += number_to_publish;
+      // 暂时不用打印
+      //=================================================================
+      // ROS_INFO(
+      //     "[Mgv Local Planner][Command Publish] Publishing %zu samples of %zu. "
+      //     "Start index: %zu Time: %f Start position: %f Start velocity: %f End "
+      //     "time: %f End position: %f",
+      //     trajectory_to_publish.size(), path_queueOFmgv_.size(), starting_index,
+      //     trajectory_to_publish.front().time_from_start_ns * 1.0e-9,
+      //     trajectory_to_publish.front().position_W.x(),
+      //     trajectory_to_publish.front().velocity_W.x(),
+      //     trajectory_to_publish.back().time_from_start_ns * 1.0e-9,
+      //     trajectory_to_publish.back().position_W.x());
+      //=================================================================
+      mgv_msgs::msgMultiDofJointTrajectoryFromEigen(trajectory_to_publish, &msg); // 转换为ros消息发出
+      command_pub_.publish(msg);                                                  // 发布控制消息
+      path_indexOFmgv_ += number_to_publish;                                      // Update the path index and notify for replanning
       should_replan_.notify();
     }
     // Does there need to be an else????
   }
+
+  void MgvLocalPlanner::visualizePath() {} // TBD
 }
